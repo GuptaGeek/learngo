47d073eb57fae03481b5c160a7f9a86ad99030ee
sheetal gupta <150527877+GuptaGeek@users.noreply.github.com>
2025-02-11 13:40:58 +0530

    Update main.go

--- 07-printf/exercises/07-print-the-type-2/main.go
+++ 07-printf/exercises/07-print-the-type-2/main.go
@@ -7,7 +7,7 @@
 // Follow me on twitter: https://twitter.com/inancgumus
 
 package main
-
+import "fmt"
 // ---------------------------------------------------------
 // EXERCISE: Print the Type #2
 //
@@ -18,5 +18,6 @@ package main
 // ---------------------------------------------------------
 
 func main() {
-	// ?
+	value:=3.14
+	fmt.Printf("Type of %.2f is %T",value,value)
 }
