11a8f5156f5e383e40b338999b06f601748ac032
sheetal gupta <150527877+GuptaGeek@users.noreply.github.com>
2025-02-11 09:58:53 +0530

    Update main.go

--- 06-variables/06-project-greeter/exercises/01-count-arguments/main.go
+++ 06-variables/06-project-greeter/exercises/01-count-arguments/main.go
@@ -7,6 +7,10 @@
 // Follow me on twitter: https://twitter.com/inancgumus
 
 package main
+import (
+	"fmt"
+	"os"
+	)
 
 // ---------------------------------------------------------
 // EXERCISE: Count the Arguments
@@ -22,8 +26,9 @@ package main
 
 func main() {
 	// UNCOMMENT & FIX THIS CODE
-	// count := ?
+	args:=os.Args
+	count :=len(args)-1 
 
 	// UNCOMMENT IT & THEN DO NOT TOUCH THIS CODE
-	// fmt.Printf("There are %d names.\n", count)
+	 fmt.Printf("There are %d names.\n", count)
 }
