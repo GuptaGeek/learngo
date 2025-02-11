8f1e51ad6b834a5b0dba4fb024d0261ea1c9fbdc
sheetal gupta <150527877+GuptaGeek@users.noreply.github.com>
2025-02-11 12:55:19 +0530

    Update main.go

--- 10-constants/exercises/08-iota-months-2/main.go
+++ 10-constants/exercises/08-iota-months-2/main.go
@@ -23,15 +23,15 @@ import "fmt"
 func main() {
 	// HINT: This is a valid constant declaration
 	//       Blank-Identifier can be used in place of a name
-	const _ = iota
+	// const _ = iota
 	//    ^- this is just a name
 
 	// Now, use iota and initialize the following constants
 	// "automatically" to 1, 2, and 3 respectively.
 	const (
-		Jan = iota
-		Feb
-		Mar
+		Jan = iota+1
+		Feb =iota+1
+		Mar=iota+1
 	)
 
 	// This should print: 1 2 3
